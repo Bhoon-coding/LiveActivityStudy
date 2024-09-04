4a373fb210dc9784cfe096867d5483da3e04fbab
bhoon-coding <bhooncoding@gmail.com>
2024-09-04 16:20:30 +0900

    Initial Commit

---
+++ LiveActivityStudy/LiveActivityStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveActivityStudyApp.swift
+//  LiveActivityStudy
+//
+//  Created by BH on 9/4/24.
+//
+
+import SwiftUI
+
+@main
+struct LiveActivityStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
